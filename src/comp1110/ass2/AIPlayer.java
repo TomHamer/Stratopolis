fbdc9ad1c78093eb879da8e8dcaabd2a1242f9c3
calum <u6044174@anu.edu.au>
2016-08-18 11:27:01 +1000

    Updated UML

--- src/comp1110/ass2/AIPlayer.java
+++
@@ -1,8 +0,0 @@
-package comp1110.ass2;
-
-/**
- * Created by calum on 8/18/2016.
- */
-public class AIPlayer {
-    public AIPlayer() {}
-}
--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -1,7 +1,7 @@
 package comp1110.ass2.gui;
 
-import comp1110.ass2.AIPlayer;
 import comp1110.ass2.BoardState;
+import comp1110.ass2.StratoGame;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -10,7 +10,6 @@ public class Board extends Application {
     private static final int BOARD_HEIGHT = 700;
     private static final int SQUARE_SIZE = 23;
     private BoardState board = new BoardState();
-    private AIPlayer p2 = new AIPlayer();
 
     // FIXME Task 8: Implement a basic playable Strato Game in JavaFX that only allows pieces to be placed in valid places
 
